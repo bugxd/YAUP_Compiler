0083a1116828e48ca2a0964439ec7182969703c4
Dozzler <doweinberger@edu.aau.at>
2018-03-14 18:14:48 +0100

    did stuff

--- src/yapl/impl/BackendMIPS.java
+++ src/yapl/impl/BackendMIPS.java
@@ -3,13 +3,16 @@ package yapl.impl;
 import static java.text.MessageFormat.format;
 
 import java.io.PrintStream;
+import java.util.Optional;
 
 public final class BackendMIPS implements yapl.interfaces.BackendAsmRM {
 
   private final PrintStream outputStream;
+  private Registers registers;
 
   public BackendMIPS (PrintStream outputStream) {
     this.outputStream = outputStream;
+    this.registers = new Registers();
 
     if (outputStream == null) {
       throw new IllegalArgumentException("outputStream must not be null!");
@@ -33,27 +36,35 @@ public final class BackendMIPS implements yapl.interfaces.BackendAsmRM {
 
   @Override
   public byte allocReg() {
-    return 0;
+    Optional<Register> registerOptional = registers.getUnusedRegister();
+    if (!registerOptional.isPresent()) {
+      return -1;
+    }
+
+    Register register = registerOptional.get();
+    register.setInUse(true);
+    return register.getRegisterNumber();
   }
 
   @Override
   public void freeReg(byte reg) {
-
+    registers.freeReqister(reg);
   }
 
   @Override
   public byte zeroReg() {
-    return 0;
+    Register zeroRegister = registers.getRegisterByNumber((byte)0);
+    return zeroRegister.getRegisterNumber();
   }
 
   @Override
   public void comment(String comment) {
-    outputStream.append(format("% {0}\n", comment));
+    outputStream.append(format("# {0}\n", comment));
   }
 
   @Override
   public void emitLabel(String label, String comment) {
-    outputStream.append(format("{0}:\t\t% {1}\n", label, comment));
+    outputStream.append(format("{0}:\t\t# {1}\n", label, comment));
   }
 
   @Override
@@ -138,32 +149,55 @@ public final class BackendMIPS implements yapl.interfaces.BackendAsmRM {
 
   @Override
   public void neg(byte regDest, byte regX) {
-
   }
 
   @Override
   public void add(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("add {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void addConst(byte regDest, byte regX, int value) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
 
+    outputStream.append(format("add {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               value));
   }
 
   @Override
   public void sub(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("sub {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void mul(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("mul {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void div(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("div {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
@@ -173,42 +207,73 @@ public final class BackendMIPS implements yapl.interfaces.BackendAsmRM {
 
   @Override
   public void isLess(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("slt {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void isLessOrEqual(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("sle {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void isEqual(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("seq {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void not(byte regDest, byte regSrc) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register source = registers.getRegisterByNumber(regSrc);
 
+    outputStream.append(format("not {0}, {1}\n", destination.getName(), source.getName()));
   }
 
   @Override
   public void and(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("and {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void or(byte regDest, byte regX, byte regY) {
+    Register destination = registers.getRegisterByNumber(regDest);
+    Register sourceX = registers.getRegisterByNumber(regX);
+    Register sourceY = registers.getRegisterByNumber(regY);
 
+    outputStream.append(format("or {0}, {1}, {2}\n", destination.getName(), sourceX.getName(),
+                               sourceY.getName() ));
   }
 
   @Override
   public void branchIf(byte reg, boolean value, String label) {
+    Register register = registers.getRegisterByNumber(reg);
 
+    outputStream.append(format("beq {0}, {1}, {2}\n", register.getName(), this.boolValue(value),
+                                   label));
   }
 
   @Override
   public void jump(String label) {
-    outputStream.append(format("jr {0}\n", label));
+    outputStream.append(format("j {0}\n", label));
   }
 
   @Override
@@ -218,7 +283,7 @@ public final class BackendMIPS implements yapl.interfaces.BackendAsmRM {
 
   @Override
   public void exitMain(String label) {
-    emitLabel(label, "main epilogue");
+    emitLabel(label, "main_epilogue");
     outputStream.append("li $v0 10\n");
     outputStream.append("syscall\n");
   }
---
+++ src/yapl/impl/Registers.java
@@ -0,0 +1,78 @@
+package yapl.impl;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public final class Registers {
+
+  private List<Register> registers;
+  
+  public Registers() {
+    this.registers = new ArrayList<>();
+    // FIXME: 14.03.18 NOT ALL REGISTERS ARE USABLE!!!
+    // TODO: 14.03.18 möglicherweise müssen wir noch Registerkategorien einfügen
+    registers.add(new Register((byte)0, "$zero", true));
+    registers.add(new Register((byte)1, "$at", true));
+    registers.add(new Register((byte)2, "$v0"));
+    registers.add(new Register((byte)3, "$v1"));
+    registers.add(new Register((byte)4, "$a0"));
+    registers.add(new Register((byte)5, "$a1"));
+    registers.add(new Register((byte)6, "$a2"));
+    registers.add(new Register((byte)7, "$a3"));
+    registers.add(new Register((byte)8, "$t0"));
+    registers.add(new Register((byte)9, "$t1"));
+    registers.add(new Register((byte)10, "$t2"));
+    registers.add(new Register((byte)11, "$t3"));
+    registers.add(new Register((byte)12, "$t4"));
+    registers.add(new Register((byte)13, "$t5"));
+    registers.add(new Register((byte)14, "$t6"));
+    registers.add(new Register((byte)15, "$t7"));
+    registers.add(new Register((byte)16, "$t8"));
+    registers.add(new Register((byte)17, "$t9"));
+    registers.add(new Register((byte)18, "$s0"));
+    registers.add(new Register((byte)19, "$s1"));
+    registers.add(new Register((byte)20, "$s2"));
+    registers.add(new Register((byte)21, "$s3"));
+    registers.add(new Register((byte)22, "$s4"));
+    registers.add(new Register((byte)23, "$s5"));
+    registers.add(new Register((byte)24, "$s6"));
+    registers.add(new Register((byte)25, "$s7"));
+    registers.add(new Register((byte)26, "$k0", true));
+    registers.add(new Register((byte)27, "$k1", true));
+    registers.add(new Register((byte)28, "$gp", true));
+    registers.add(new Register((byte)29, "$sp", true));
+    registers.add(new Register((byte)30, "$fp", true));
+    registers.add(new Register((byte)31, "$ra", true));
+  }
+
+  public Optional<Register> getUnusedRegister() {
+    for (Register register : registers) {
+      if (!register.isInUse()) {
+        return of(register);
+      }
+    }
+    return empty();
+  }
+
+  public Register getRegisterByNumber (byte registerNumber) {
+    for (Register register : registers) {
+      if (register.getRegisterNumber() == registerNumber) {
+        return register;
+      }
+    }
+
+    throw new IllegalArgumentException("registerNumber must be between 0 and 31");
+  }
+
+  public void freeReqister(byte reg) {
+    for (Register register : registers) {
+      if (register.getRegisterNumber() == reg)  {
+        register.setInUse(false);
+      }
+    }
+  }
+}
