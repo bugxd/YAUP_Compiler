f341ebebd56beba3300b1357a0c00e59a3d160e1
Dozzler <dozzler93@gmail.com>
2018-04-07 02:47:51 +0200

    adds enum SyscallCode

---
+++ src/yapl/impl/SyscallCode.java
@@ -0,0 +1,32 @@
+package yapl.impl;
+
+/**
+ * Created by Dominic on 06.04.2018.
+ */
+public enum SyscallCode {
+    PRINT_INT(1),
+    PRINT_FLOAT(2),
+    PRINT_DOUBLE(3),
+    PRINT_STRING(4),
+    READ_INT(5),
+    READ_FLOAT(6),
+    READ_DOUBLE(7),
+    READ_STRING(8),
+    ALLOCATE_HEAP(9),
+    EXIT(10);
+
+    private final int value;
+
+    private SyscallCode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+}
